78fa1840d21a66b7fd462c45f84a7ba362cdc05a
ベニック <62417825+Venicck@users.noreply.github.com>
2024-09-09 20:36:05 +0900

    index.js の更新
    visibilityからdisplayによる表示の切り替えで謎の空白に対処

--- index.js
+++ index.js
@@ -50,9 +50,9 @@ function search(num){
             dis_number.textContent = num[0] + "F";
             dis_number.style.background=colors[num[0] - 1];
             if (data[num][4] != undefined)
-                document.getElementById("info-btn").style.visibility = "visible";
+                document.getElementById("info-btn").style.display = "inline";
             else
-                document.getElementById("info-btn").style.visibility = "hidden";
+                document.getElementById("info-btn").style.display = "hidden";
         }
     }
 }
