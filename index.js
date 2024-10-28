a1647c95f1e74cccb9bda3aaa248d60ea32d98e4
LisaNavi <LisaSchNavi@gmail.com>
2024-10-28 09:41:25 +0900

    infoボタンを常に表示

--- index.js
+++ index.js
@@ -69,6 +69,9 @@ function info(num) {
         desk.textContent = "この教室のインフォメーションはありません";
     }
 
+    if (Number.isInteger(num)){
+        document.getElementById("information").style.visibility = "hidden";
+    }
 
     
 }
